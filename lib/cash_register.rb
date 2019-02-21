cc6edb1dbcc1f92fe9df1073660d93097ce5bdf7
Bry Cox <carl.cc@gmail.com>
2019-02-21 20:23:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
       @title = title
       @price = price
       @quantity = quantity
-      @items << self.title
+      @items = title
       self.total += price*quantity
   end
 
